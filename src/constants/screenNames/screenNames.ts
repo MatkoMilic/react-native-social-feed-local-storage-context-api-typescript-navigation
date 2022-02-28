3a4abb1d6a54e9d197e26a91f14f8d610542a553
Matko Milić <Matko Mili@S-Soft.local>
2022-02-28 17:45:57 +0100

    Add screen names strings that are needed in navigation.

---
+++ src/constants/screenNames/screenNames.ts
@@ -0,0 +1,8 @@
+import {
+  MainStackNameType,
+  OnboardingStackNameType,
+} from "../navigatorTypes/navigatorTypes";
+
+export const FEED_SCREEN: MainStackNameType = "FeedScreen";
+export const LOADING_SCREEN: OnboardingStackNameType = "LoadingScreen";
+export const LOGIN_SCREEN: OnboardingStackNameType = "LoginScreen";
