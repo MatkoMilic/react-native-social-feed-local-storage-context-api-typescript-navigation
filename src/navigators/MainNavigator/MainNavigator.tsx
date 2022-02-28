0206fc495adbe7f5a576523847113726a5dcd2bd
Matko Milić <Matko Mili@S-Soft.local>
2022-02-28 17:34:39 +0100

    Add MainNavigator, define its screens.

---
+++ src/navigators/MainNavigator/MainNavigator.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import { FeedScreen } from "../../screens";
+import { MainStackParamList, FEED_SCREEN } from "../../constants";
+
+interface MainNavigatorProps {
+  navigation: NativeStackNavigationProp<MainStackParamList>;
+}
+
+const MainStack = createNativeStackNavigator();
+
+const MainNavigator: FC<MainNavigatorProps> = ({ navigation }) => {
+  return (
+    <MainStack.Navigator
+      //screenOptions={{headerShown: false}}
+      initialRouteName={FEED_SCREEN}
+    >
+      <MainStack.Screen name={FEED_SCREEN} component={FeedScreen} />
+    </MainStack.Navigator>
+  );
+};
+
+export default MainNavigator;
