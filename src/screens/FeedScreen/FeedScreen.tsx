61568f7882e91ad9b68225401ee472bb8e3e72a2
Matko Milić <Matko Mili@S-Soft.local>
2022-02-28 17:48:00 +0100

    Add Feed Screen.

---
+++ src/screens/FeedScreen/FeedScreen.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { View } from "react-native";
+import { IMainNavScreenProps } from "../../types/IMainNavScreenProps/IMainNavScreenProps";
+import { ScreenContainer } from "../../components";
+import { styles } from "./style";
+
+interface FeedScreenProps extends IMainNavScreenProps {}
+
+const FeedScreen: React.FC<FeedScreenProps> = ({ navigation }) => {
+  return (
+    <ScreenContainer>
+      <View></View>
+    </ScreenContainer>
+  );
+};
+
+export default FeedScreen;
