4614dd23ab416cd9f040134f9acb106f9db3bbd2
Matko Milić <Matko Mili@S-Soft.local>
2022-02-28 17:30:44 +0100

    Add navigator types.

---
+++ src/constants/navigatorTypes/navigatorTypes.ts
@@ -0,0 +1,16 @@
+import { NavigatorScreenParams } from "@react-navigation/native";
+
+export type OnboardingStackParamList = {
+  LoadingScreen: undefined;
+  LoginScreen: undefined;
+};
+export type MainStackParamList = {
+  FeedScreen: undefined;
+};
+export type RootNavigatorParamsList = {
+  MainNavigator: NavigatorScreenParams<MainStackParamList>;
+  OnboardingNavigator: NavigatorScreenParams<OnboardingStackParamList>;
+};
+export type RootNavigatorNameType = keyof RootNavigatorParamsList;
+export type MainStackNameType = keyof MainStackParamList;
+export type OnboardingStackNameType = keyof OnboardingStackParamList;
