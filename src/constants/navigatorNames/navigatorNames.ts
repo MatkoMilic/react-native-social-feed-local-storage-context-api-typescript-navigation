dc8bbcbeea5aa0dd4a525802eca874ea8b827d9d
Matko Milić <Matko Mili@S-Soft.local>
2022-02-28 17:33:19 +0100

    Add navigator names.

---
+++ src/constants/navigatorNames/navigatorNames.ts
@@ -0,0 +1,11 @@
+import {RootNavigatorNameType} from '../navigatorTypes/navigatorTypes';
+
+interface INavigatorNames {
+  MAIN_NAVIGATOR: RootNavigatorNameType;
+  ONBOARDING_NAVIGATOR: RootNavigatorNameType;
+}
+
+export const navigatorNames: INavigatorNames = {
+  MAIN_NAVIGATOR: 'MainNavigator',
+  ONBOARDING_NAVIGATOR: 'OnboardingNavigator',
+};
