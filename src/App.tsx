e24e1c691206ed509bdd99e6fcca828fb8744b8b
Matko Milić <Matko Mili@S-Soft.local>
2022-02-28 17:40:13 +0100

    Add RootNavigator and wrap it with NavigationProvider.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { RootNavigator } from "./navigators";
+import { NavigationProvider } from "./components";
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationProvider>
+      <RootNavigator />
+    </NavigationProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "center",
-  },
-});
+export default App;
