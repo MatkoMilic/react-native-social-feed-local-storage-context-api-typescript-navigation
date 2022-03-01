bf10ed3cec1375cdad59844832f468544a6c481e
Matko Milić <Matko Mili@S-Soft.local>
2022-03-01 15:51:10 +0100

    Add image picker logic.

--- src/screens/FeedScreen/FeedScreen.tsx
+++ src/screens/FeedScreen/FeedScreen.tsx
@@ -1,15 +1,37 @@
-import React, { useState } from 'react';
-import { View } from 'react-native';
-import { IMainNavScreenProps } from '../../types/IMainNavScreenProps/IMainNavScreenProps';
-import { ScreenContainer } from '../../components';
-import { styles } from './style';
+import React, { useState } from "react";
+import { Button, Image, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { ScreenContainer } from "../../components";
+import { IMainNavScreenProps } from "../../types";
 
 interface FeedScreenProps extends IMainNavScreenProps {}
 
 const FeedScreen: React.FC<FeedScreenProps> = ({ navigation }) => {
+  const [image, setImage] = useState<any>();
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return (
     <ScreenContainer>
-      <View></View>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && (
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        )}
+      </View>
     </ScreenContainer>
   );
 };
