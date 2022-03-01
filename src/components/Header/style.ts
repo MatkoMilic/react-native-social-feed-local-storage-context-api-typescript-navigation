4e11eb30f6734d1a95b082e109239d86f5170ab7
Matko Milić <Matko Mili@S-Soft.local>
2022-03-01 17:50:32 +0100

    Add styles for Header.

---
+++ src/components/Header/style.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  headerContent: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#397af8",
+    marginBottom: 20,
+    width: "100%",
+    paddingVertical: 15,
+  },
+  heading: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+});
