c6a61f2b292698faecf58d28948e8d22daf29e1f
Matko Milić <Matko Mili@S-Soft.local>
2022-03-01 17:53:41 +0100

    Add new styles.

--- src/screens/CreatePostScreen/style.ts
+++ src/screens/CreatePostScreen/style.ts
@@ -5,4 +5,13 @@ export const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  image: {
+    width: 200,
+    height: 200,
+  },
+  imageAndTextContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
