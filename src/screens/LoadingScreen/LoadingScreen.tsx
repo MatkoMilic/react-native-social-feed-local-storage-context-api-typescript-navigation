f398ec88ea565fbf07bd64a127a782143dacf25b
Matko Milić <Matko Mili@S-Soft.local>
2022-03-01 15:50:46 +0100

    Refactoring.

--- src/screens/LoadingScreen/LoadingScreen.tsx
+++ src/screens/LoadingScreen/LoadingScreen.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { StatusBar } from 'expo-status-bar';
-import { LOGIN_SCREEN, navigatorNames } from '../../constants';
-import { IOnboardingNavScreenProps } from '../../types';
-import { ScreenContainer } from '../../components';
-import style from './style';
+import React, { useEffect } from "react";
+import { Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { StatusBar } from "expo-status-bar";
+import { LOGIN_SCREEN, navigatorNames } from "../../constants";
+import { IOnboardingNavScreenProps } from "../../types";
+import { ScreenContainer } from "../../components";
+import { styles } from "./style";
 
 interface LoadingScreenProps extends IOnboardingNavScreenProps {}
 
@@ -14,7 +14,7 @@ const LoadingScreen: React.FC<LoadingScreenProps> = ({ navigation }) => {
     // if we add autentification system here is where logic for re routing will be,
     // or some other check before starting the app, for now user will be automatically
     // sent to login screen where he can press a button to go to app FEED.
-    navigation.navigate('LoginScreen');
+    navigation.navigate("LoginScreen");
   };
 
   useEffect(() => {
