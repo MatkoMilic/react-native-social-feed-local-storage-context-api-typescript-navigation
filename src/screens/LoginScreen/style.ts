7adce5c8a197d2bfab84387a596629b9269dc40c
Matko Milić <Matko Mili@S-Soft.local>
2022-03-01 17:51:52 +0100

    Add new styles for Login Screen.

--- src/screens/LoginScreen/style.ts
+++ src/screens/LoginScreen/style.ts
@@ -13,4 +13,18 @@ export const styles = StyleSheet.create({
     width: "100%",
     marginVertical: 20,
   },
+  titleStyle: {
+    fontWeight: "500",
+  },
+  buttonStyle: {
+    backgroundColor: "rgba(199, 43, 98, 1)",
+    borderColor: "transparent",
+    borderWidth: 0,
+  },
+  buttonContainerStyle: {
+    width: 200,
+    height: 45,
+    marginHorizontal: 50,
+    marginVertical: 10,
+  },
 });
