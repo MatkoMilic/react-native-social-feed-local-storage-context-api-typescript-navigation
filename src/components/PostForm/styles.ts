1eb0a6bbae2401a272d9ff0f844e7a201dde92f1
Matko Milić <Matko Mili@S-Soft.local>
2022-03-02 15:56:17 +0100

    Add styles for PostForm component.

---
+++ src/components/PostForm/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  postFormContainer: {
+    flex: 1,
+  },
+  inputErrorMessage: {
+    color: "red",
+  },
+  postButton: {
+    backgroundColor: "red",
+  },
+});
