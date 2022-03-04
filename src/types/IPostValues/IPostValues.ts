c3757016a8f019f2912e837d6cccf24ce460bf25
Matko Milić <Matko Mili@S-Soft.local>
2022-03-04 10:39:10 +0100

    Add new member of interface.

--- src/types/IPostValues/IPostValues.ts
+++ src/types/IPostValues/IPostValues.ts
@@ -1,9 +1,11 @@
 export interface IPostValues {
   postImage: string;
   postDescription: string;
+  uniquePostID: string;
 }
 
 export const initialPostValues = (): IPostValues => ({
   postImage: "",
   postDescription: "",
+  uniquePostID: "",
 });
