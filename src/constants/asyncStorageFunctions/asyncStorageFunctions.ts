4bc8d5c6747c2ff2aea6b41ff77302302eb47686
Matko Milić <Matko Mili@S-Soft.local>
2022-03-04 10:44:04 +0100

    Add helping AS functions.

---
+++ src/constants/asyncStorageFunctions/asyncStorageFunctions.ts
@@ -0,0 +1,25 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { IPostValues, initialPostValues } from "../../types";
+
+export const getAllAsyncStorageData = async () => {
+  try {
+    const allItemsFromAS: IPostValues = initialPostValues();
+    const keys = await AsyncStorage.getAllKeys();
+    for (const key of keys) {
+      const val = await AsyncStorage.getItem(key);
+      if (val) {
+        allItemsFromAS[key as keyof IPostValues] = val;
+      }
+    }
+    const result = Object.keys(allItemsFromAS);
+    return (
+      console.log("All items: ", allItemsFromAS), console.log(typeof result)
+    );
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export const clearEntireAsyncStorage = async () => {
+  await AsyncStorage.clear();
+};
