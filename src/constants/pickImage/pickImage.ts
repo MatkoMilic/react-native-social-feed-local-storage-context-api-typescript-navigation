6151ef5352d55e4a971220f989bc034d3d253479
Matko Milić <Matko Mili@S-Soft.local>
2022-03-04 10:44:19 +0100

    Add picking image function.

--- src/constants/pickImage/pickImage.ts
+++ src/constants/pickImage/pickImage.ts
@@ -1,25 +1,24 @@
 import * as ImagePicker from "expo-image-picker";
 
-export const pickImage = async (handleChange: {
-  (e: React.ChangeEvent<any>): void;
-  <T = string | React.ChangeEvent<any>>(
-    field: T
-  ): T extends React.ChangeEvent<any>
-    ? void
-    : (e: string | React.ChangeEvent<any>) => void;
-}) => {
-  let result = await ImagePicker.launchImageLibraryAsync({
+export const pickImage = async (
+  handleChange: {
+    (e: React.ChangeEvent<any>): void;
+    <T = string | React.ChangeEvent<any>>(
+      field: T
+    ): T extends React.ChangeEvent<any>
+      ? void
+      : (e: string | React.ChangeEvent<any>) => void;
+  },
+  setImage: React.Dispatch<React.SetStateAction<string>>
+) => {
+  const result = await ImagePicker.launchImageLibraryAsync({
     mediaTypes: ImagePicker.MediaTypeOptions.All,
     allowsEditing: true,
     aspect: [4, 3],
     quality: 1,
   });
-
-  //I will for now leave this console.log as its useful
-  //for us to see the functionality of this demo app
-  console.log(result);
-
   if (!result.cancelled) {
     handleChange(result.uri);
+    setImage(result.uri);
   }
 };
