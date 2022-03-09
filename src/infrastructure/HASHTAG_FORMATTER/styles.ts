f740b79fccbaae00ed283aa0b520d1fb6ef35ea1
Matko Milić <Matko Mili@S-Soft.local>
2022-03-09 13:19:57 +0100

    Commit deleteed files.

--- src/infrastructure/HASHTAG_FORMATTER/styles.ts
+++
@@ -1,7 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const styles = StyleSheet.create({
-  textFormatterColor: {
-    color: "#0000FF",
-  },
-});
--- src/utils/useOnSubmit/index.ts
+++
@@ -1,1 +0,0 @@
-export {default as useOnSubmit} from './useOnSubmit';
